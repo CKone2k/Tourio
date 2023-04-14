76af76f1699827e786d12fefd1cf764b2e02e503
Cüneyd Kasap <Kasapcuneyd1@gmail.com>
2023-04-14 12:05:35 +0200

    tourio

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,23 @@
-import { places } from '../../../../lib/db.js';
+import Place from "../../../../db/models/Place";
+import dbConnect from "../../../../db/connect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+  if (request.method === "GET") {
+    const places = await Place.findById(id);
 
-  if (!id) {
-    return;
+    return response.status(200).json(places);
   }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
-
-  response.status(200).json(place);
 }
