60cd9660f7bf5c715e7212c8122ce4a4b3b6f56a
Cüneyd Kasap <Kasapcuneyd1@gmail.com>
2023-04-14 14:46:01 +0200

    yeet

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,20 @@
-import Place from "../../../../db/models/Place";
+import Place from "../../../../db/models/place";
 import dbConnect from "../../../../db/connect";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
-  if (request.method === "GET") {
-    const places = await Place.findById(id);
 
-    return response.status(200).json(places);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(place);
   }
+
   if (request.method === "POST") {
     try {
       const placeData = request.body;
@@ -19,5 +25,17 @@ export default async function handler(request, response) {
       console.log(error);
       response.status(400).json({ error: error.message });
     }
+  }
+  if (request.method === "PUT") {
+    await Place.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+
+    response.status(200).json({ status: `Place ${id} updated!` });
+  }
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+
+    response.status(200).json({ status: `Place ${id} successfully deleted.` });
   }
 }
